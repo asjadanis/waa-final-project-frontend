aba7d250951b69e7ba8e1f6aba7cde1857e6779c
asjadanis <asjadanis128@gmail.com>
2022-09-22 20:53:29 -0500

    fix: switched to hash-router for github pages

--- src/components/RoutesWrapper/index.jsx
+++ src/components/RoutesWrapper/index.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
 import Layout from "../Layout";
 
 const RoutesWrapper = () => {
   return (
-    <BrowserRouter>
+    // Using HashRouter because BrowserRouter doesn't work with github pages.
+    <HashRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
